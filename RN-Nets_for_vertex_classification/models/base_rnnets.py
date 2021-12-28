247363d7c7faba124342b34576dad997f01ada41
Wei Ye <weiye@cs.ucsb.edu>
2021-12-28 13:19:08 +0800

    Update base_rnnets.py

--- RN-Nets_for_vertex_classification/models/base_rnnets.py
+++ RN-Nets_for_vertex_classification/models/base_rnnets.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 class BaseRNNETS:
     def loss(logits, labels, nb_classes, class_weights):
